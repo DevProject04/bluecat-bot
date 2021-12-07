1fbd7e280d99e24d34b84d6c54d78738fb487555
DEV_Project <me@projecttl.net>
2021-12-07 11:11:21 +0900

    feat: invite command

---
+++ src/commands/invite.js
@@ -0,0 +1,16 @@
+const { MessageEmbed } = require("discord.js");
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('invite')
+		.setDescription('You can get this bot invite url!'),
+	async execute(interaction) {
+        const pingEmbed = new MessageEmbed()
+            .setTitle(":mailbox_with_mail: Invite Code!")
+            .setDescription(`https://projecttl.net/invite/bluecat`)
+            .setColor("BLUE")
+            .setFooter(`${interaction.user.username}#${interaction.user.discriminator}`, interaction.user.avatarURL());
+		await interaction.reply({ embeds: [pingEmbed] });
+	},
+};
