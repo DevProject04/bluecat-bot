8c3b74da896655d3a6b67b64d85a36719d543080
Dev_Project <me@projecttl.net>
2021-12-17 10:31:21 +0900

    feat: app.js => app.ts

---
+++ app.ts
@@ -0,0 +1,67 @@
+const fs = require('fs');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { Client, Collection, Intents } = require('discord.js');
+const { clientId, token } = require('./src/config/config.json');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_BANS] });
+client.commands = new Collection();
+
+class Bot {
+	commands = []
+	init() {
+		this.loadEvnet();
+		this.loadCommand();
+		this.registerCommand();
+	}
+
+	loadEvnet() {
+		const eventFiles = fs.readdirSync('./src/events').filter(file => file.endsWith('.js'));
+
+		for (const file of eventFiles) {
+			const event = require(`./src/events/${file}`);
+			if (event.once) {
+				client.once(event.name, (...args) => event.execute(...args));
+			} else {
+				client.on(event.name, (...args) => event.execute(...args));
+			}
+
+			console.log(`event ${file} loaded!`);
+		}
+	}
+
+	loadCommand() {
+		const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
+
+		for (const file of commandFiles) {
+			const command = require(`./src/commands/${file}`);
+			console.log(`Loaded ${command.data.name}!`);
+			client.commands.set(command.data.name, command);
+			this.commands.push(command.data.toJSON());
+		}
+	}
+
+	registerCommand() {
+		const rest = new REST({ version: '9' }).setToken(token);
+
+		(async () => {
+			try {
+				console.log('Started refreshing application (/) commands.');
+
+				await rest.put(
+					Routes.applicationCommands(clientId),
+					{ body: this.commands },
+				);
+
+				console.log('Successfully reloaded application (/) commands.');
+			} catch (error) {
+				console.error(error);
+			}
+		})();
+	}
+}
+
+const bot = new Bot();
+bot.init();
+
+client.login(token);
