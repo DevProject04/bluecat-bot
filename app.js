f3f3ff6c6679e10839c45f71101e180cb042cb56
Project_TL <me@projecttl.net>
2022-06-03 11:27:55 +0900

    feat: add intents

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { Client, Collection, Intents, Interaction } = require('discord.js');
+const { Client, Collection, Intents } = require('discord.js');
 const { clientId, token } = require('./config.json');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_BANS] });
+const client = new Client({
+	intents: [
+		Intents.FLAGS.GUILDS,
+		Intents.FLAGS.GUILD_MESSAGES,
+		Intents.FLAGS.DIRECT_MESSAGES,
+		Intents.FLAGS.GUILD_PRESENCES,
+		Intents.FLAGS.GUILD_BANS
+	],
+	partials: [
+		"CHANNEL"
+	]
+});
 client.commands = new Collection();
 
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
